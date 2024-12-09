a8d4c27905c2d6918541ddb84270769660eca683
Ehosanul Islam Sazit <sazitislam96@gmail.com>
2024-12-09 18:15:16 +0600

    Fixed logo loading issue by using alternative file referencing for public assets. Ensured compatibility for both local development and GitHub Pages deployment.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import cookingLogo from "../assets/cooking.png"
 export default function Header() {
     return (
         <header className="header">
-          <img className="header-logo" src="/cooking.png" alt="header logo" />
-          <h1 className="header-title">Claude Chef</h1>
+            <img className="header-logo" src={cookingLogo} alt="header logo" />
+            <h1 className="header-title">Claude Chef</h1>
         </header>
-      );
+    );
 }
